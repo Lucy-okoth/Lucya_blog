f0c2981acb430a7cb3c58874c509cc4d9c53d1b9
Lucy okoth <lucyakokoth@gmail.com>
2022-12-04 10:19:06 +0300

    Modified name

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
    const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'lucya', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'lucya', id: 3 }
 
    ]);
    const handleDelete = (id) => {
@@ -14,7 +14,7 @@ const Home = () => {
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-           <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="mario's Blogs" />
+           <BlogList blogs={blogs.filter((blog) => blog.author === "lucya")} title="lucya's Blogs" />
         </div>
      );
 }
