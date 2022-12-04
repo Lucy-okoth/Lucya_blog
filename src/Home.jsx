7db8b417ed226b94dd56fab6eea8d610a61eff03
Lucy okoth <lucyakokoth@gmail.com>
2022-12-04 12:40:15 +0300

    Added conditional loading message

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
    const [blogs, setBlogs] = useState(null);
+   const [isPending, setIsPending] = useState(true);
 
-   const [name, setName] = useState("Lucya")
+//    const [name, setName] = useState("Lucya")
 
 
 //    const handleDelete = (id) => {
@@ -12,18 +13,22 @@ const Home = () => {
 //    }
 
    useEffect (() =>{
-    fetch(" http://localhost:8000/blogs")
+    setTimeout(() => {
+        fetch(" http://localhost:8000/blogs")
     .then(res => {
     return res.json();
     })
     .then (data => {
-        console.log(data);
+        // console.log(data);
         setBlogs(data);
+        setIsPending(false);
 
     })
+    }, 1000);
    }, []);
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
 
            
